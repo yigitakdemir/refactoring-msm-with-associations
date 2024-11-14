c4988928eb1254b748c0e58f1be3bf27b7c01cc2
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-11-14 00:14:47 +0000

    7w 1814

--- app/models/director.rb
+++ app/models/director.rb
@@ -12,4 +12,7 @@
 #
 class Director < ApplicationRecord
   validates(:name, presence: true)
+
+  has_many(:filmography, class_name: "Movie", foreign_key: "director_id")
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,4 +15,15 @@
 class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
+
+  belongs_to(:director, class_name: "Director", foreign_key: "director_id")
+#  def director
+#    d_id = self.director_id
+#
+#    matching_directors = Director.where({ :id => d_id })
+#
+#    the_director = matching_directors.first
+#
+#    return the_director
+#  end
 end
